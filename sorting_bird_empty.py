879f1f4c4bfd4e0288728cd55315b15d9869ecf7
Sarah Kennelly <sek40@uclive.ac.nz>
2020-05-16 17:00:48 +1200

    Added a function to print all the totals for files

--- sorting_bird_empty.py
+++ sorting_bird_empty.py
@@ -75,11 +75,11 @@ def sort_train_test_bird_parakeet():
 
 			copy_files(src_path, train_path, test_path, 80)
 
-def sort_bird_parakeet_empty(dates, dest_path):
-	sorts = [("NPP/Birds", "bird"), ("", "parakeet"), ("NPP/Empty", "empty")]
+def sort_training_testing(dates, dest_path, feeder, sorts):
+	
 	for date in dates:
 		for path, type_file in sorts:
-			src_path = os.path.join("F010 [SBH]/", date, path)
+			src_path = os.path.join(feeder, date, path)
 			test_path = os.path.join(dest_path, "testing/", type_file)
 			train_path = os.path.join(dest_path, "training/", type_file)
 			print(src_path)
@@ -87,33 +87,25 @@ def sort_bird_parakeet_empty(dates, dest_path):
 			print(train_path)
 			print("")
 
-			#copy_files(src_path, train_path, test_path, 80)
+			copy_files(src_path, train_path, test_path, 80)
 
 
 def print_file_total(path, name):
 	files = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
 	print(f"{name}: {len(files)}")
 
-def print_file_totals():
-	src_dir = os.path.join(EXTERNAL_DRIVE_DIR, "Feeder10_bird_empty/")
-	test_bird = os.path.join(src_dir, "testing/bird")
-	test_empty = os.path.join(src_dir, "testing/empty")
-
-	train_bird = os.path.join(src_dir, "training/bird")
-	train_empty = os.path.join(src_dir, "training/empty")
-
-	training_dir = os.path.join(src_dir, "training/training_data/")
-	train_train_bird = os.path.join(training_dir, "train/bird")
-	train_train_empty = os.path.join(training_dir, "train/empty")
-	train_val_bird = os.path.join(training_dir, "val/bird")
-	train_val_empty = os.path.join(training_dir, "val/empty")
-
-	directory_names = [(train_bird, "General Training Bird"), (train_empty, "General Training Empty"),
-							(test_bird, "General Testing Bird"), (test_empty, "General Testing Empty"),
-							(train_train_bird, "Training (train) Bird"), (train_train_empty, "Training (train) Empty"),
-							(train_val_bird, "Training (val) Bird"), (train_val_empty, "Training (val) Empty")]
-	for path, name in directory_names:
-		print_file_total(path, name)
+def print_file_totals(directory, type_files):
+	src_dir = os.path.join(EXTERNAL_DRIVE_DIR, directory)
+	for file_type in type_files:
+		test_path = os.path.join(src_dir, "testing/", file_type)
+		print_file_total(test_path, f"Testing {file_type}")
+		train_path = os.path.join(src_dir, "training/", file_type)
+		print_file_total(train_path, f"Training {file_type}")
+		training_dir = os.path.join(src_dir, "training/training_data/")
+		train_train = os.path.join(training_dir, "train/", file_type)
+		print_file_total(train_train, f"Training (train) {file_type}")
+		train_val = os.path.join(training_dir, "val/", file_type)
+		print_file_total(train_val, f"Training (val) {file_type}")
 
 
 
@@ -128,16 +120,40 @@ def sort_testing_validation(src_dir, type_files):
 		print(test_path)
 		print("")
 
-		#copy_files(src_path, train_path, test_path, 80)
+		copy_files(src_path, train_path, test_path, 80)
 
+def print_directory_totals(ext_dir, list_dirs):
+	print(ext_dir)
+	for path, type_files in list_dirs:
+		print(path)
+		print_dir = os.path.join(ext_dir, path)
+		print_file_totals(print_dir, type_files)
+		print()
+
+def print_f10_directory_totals():
+	bird_parakeet = ("Feeder10_bird_parakeet", ["bird", "parakeet"])
+	bird_empty = ("Feeder10_bird_empty", ["bird", "empty"])
+	bird_parakeet_empty = ("Feeder10_bird_parakeet_empty", ["bird", "parakeet", "empty"])
+	print_directory_totals("Feeder10_sorted/", [bird_parakeet, bird_empty, bird_parakeet_empty])
+
+def print_f11_directory_totals():
+	bird_parakeet = ("Feeder11_bird_parakeet", ["bird", "parakeet"])
+	bird_empty = ("Feeder11_bird_empty", ["bird", "empty"])
+	bird_parakeet_empty = ("Feeder11_bird_parakeet_empty", ["bird", "parakeet", "empty"])
+	print_directory_totals("Feeder11_sorted/", [bird_parakeet, bird_empty, bird_parakeet_empty])
 
 def main():
-	dates = ["2020-01-29 to 2020-02-20 DONE HH - filtered HH 06-04-20/", 
-	"2020-02-20 to 2020-03-04 DONE HH - filtered HH 07-04-20/",
-	"2020-03-04 to 2020-03-19 DONE KT - filtered RW 03-04-20/"]
-	dest_path = "Feeder10_sorted/Feeder10_bird_parakeet_empty/"
-	sort_bird_parakeet_empty(dates, dest_path)
-	sort_testing_validation("Feeder10_sorted/Feeder10_bird_parakeet_empty/training/", ["bird", "parakeet", "empty"])
+	#dates = ["2020-01-29 to 2020-02-20 DONE HH - filtered HH 06-04-20/", 
+	#"2020-02-20 to 2020-03-04 DONE HH - filtered HH 07-04-20/",
+	#"2020-03-04 to 2020-03-19 DONE KT - filtered RW 03-04-20/"]
+	#dest_path = "Feeder10_sorted/Feeder10_bird_parakeet_empty/"
+	#dates = ["2020-01-28 to 2020-02-20 DONE HH - filtered HH 07-04-20",
+	#"2020-02-20 to 2020-03-04 DONE HH - filtered HH 07-04-20",
+	#"2020-03-04 to 2020-03-18 DONE KT - filtered RW 06-04-20"]
+	#dest_path = "Feeder11_sorted/Feeder11_bird_parakeet/"
+	#sorts = [("NPP/Birds", "bird"), ("", "parakeet")]
+	#sort_training_testing(dates, dest_path, "F011 [SBH]/", sorts)
+	#sort_testing_validation("Feeder11_sorted/Feeder11_bird_parakeet/training/", ["bird", "parakeet"])
 	
 	
 main()
